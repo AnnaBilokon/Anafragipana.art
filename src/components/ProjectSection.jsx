d9998c5730ce05162ae0f52054b0ad98c5cd228c
AnnaBilokon <Agnewka89@gmail.com>
2024-01-22 14:57:58 +0100

    change style of the text

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -28,10 +28,10 @@ const projectsData = [
 const ProjectSection = () => {
   return (
     <section id="projects">
-      <h2 className="text-center text-4xl font-bold text-black mt-8 mb-8 md:mb-12 ">
+      <h2 className="text-center text-4xl font-bold text-indigo mt-4 mb-8 md:mb-12 ">
         My projects
       </h2>
-      <div className="grid xl:grid-cols-3 justify-items-center gap-8 md:gap-12 ">
+      <div className="w-full grid grid-cols-3 justify-items-center gap-8 md:gap-12 ">
         {projectsData.map((project) => (
           <ProjectCard
             key={project.id}
